1b8c3fa8ab1147a128d833ff3fa34205e98a9bf8
Adidev-KGP <74253561+Adidev-KGP@users.noreply.github.com>
2022-05-22 16:45:57 +0530

    Update minimum_subset_sum_difference.cpp

--- minimum_subset_sum_difference.cpp
+++ minimum_subset_sum_difference.cpp
@@ -1,7 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int subsetsum(int arr[],int sum,int n){
+void solve(){
+    int n;
+    cin>>n;
+    int arr[n];
+    int sum=0;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        sum+=arr[i];
+    }
     int dp[n+1][sum+1];
     memset(dp,-1,sizeof(dp));
     for(int i=0;i<n+1;i++){
@@ -16,28 +24,15 @@ int subsetsum(int arr[],int sum,int n){
             if(arr[i-1]<=j)dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
             else dp[i][j]=dp[i-1][j];
         }
-    }return dp[n][sum];
-}
-
-
-void solve(){
-    int n;
-    cin>>n;
-    int arr[n];
-    int c=0;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-        c+=arr[i];
     }
     int ans=INT_MAX;
-    for(int i=0;i<=c;i++){
-        if(subsetsum(arr,i,n)){
-            ans=min(ans,abs(c-2*i));
+    for(int i=0;i<=sum/2;i++){
+        if(dp[n][i])ans=min(ans,abs(sum-2*i));
     }
-  }cout<<ans;
+    cout<<ans;
 }
 
 signed main(){
-   
+    
     solve();
 }
