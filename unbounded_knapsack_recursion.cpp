884dd22313703518dbd23e6aa410a3a32ad0fcab
Adidev-KGP <74253561+Adidev-KGP@users.noreply.github.com>
2022-05-23 12:12:55 +0530

    Create unbounded_knapsack_recursion.cpp

---
+++ unbounded_knapsack_recursion.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int ub_knapsack(int wt[],int val[],int w,int n){
+    if(n==0 || w==0)return 0;
+    
+    if(wt[n-1]<=w)return max(val[n-1]+ub_knapsack(wt,val,w-wt[n-1],n),ub_knapsack(wt,val,w,n-1));
+    else return ub_knapsack(wt,val,w,n-1);
+}
+
+void solve(){
+   int n;cin>>n;
+   int w;cin>>w;
+   int wt[n];int val[n];
+   for(int i=0;i<n;i++)cin>>wt[i];
+   for(int i=0;i<n;i++)cin>>val[i];
+   cout<<ub_knapsack(wt,val,w,n);
+}
+
+signed main(){
+    
+    solve();
+}
