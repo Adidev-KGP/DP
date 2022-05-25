3241658669cc4b7e448e66c4d07fe1cd2725a5bf
Adidev-KGP <74253561+Adidev-KGP@users.noreply.github.com>
2022-05-25 20:08:02 +0530

    Create longest_common_subsequence.cpp

---
+++ longest_common_subsequence.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lcs(string x,string y,int n,int m){
+    if(n==0 || m==0)return 0;
+    
+    if(x[n-1]==y[m-1])return 1+lcs(x,y,n-1,m-1);
+    else{
+        return max(lcs(x,y,n-1,m),lcs(x,y,n,m-1));
+    }
+}
+
+
+
+void solve(){
+    string x;cin>>x;
+    string y;cin>>y;
+    int n;cin>>n;
+    int m;cin>>m;
+    cout<<lcs(x,y,n,m);
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);//FASTIO
+    
+    //int _t;cin>>_t;while(_t--)  //to run solve() t no. of times where t is no. of testcases
+    solve();
+}
