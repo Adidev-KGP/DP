8b9afe6607fc3e825a5fd788ae5a98fb5315fc6c
Adidev-KGP <74253561+Adidev-KGP@users.noreply.github.com>
2022-06-01 15:06:46 +0530

    Create sequence_pattern_matching.cpp

---
+++ sequence_pattern_matching.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    string x;cin>>x;
+    string y;cin>>y;
+    int n=x.length();
+    int m=y.length();
+    
+    int dp[n+1][m+1];
+    
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<m+1;j++){
+            if(i==0 || j==0)dp[i][j]=0;
+        }
+    }
+    
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<m+1;j++){
+            if(x[i-1]==y[j-1])dp[i][j]=1+dp[i-1][j-1];
+            else dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+        }
+    }if(dp[n][m]==n)cout<<"YES";
+    else cout<<"NO";
+  
+    
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);//FASTIO
+    
+    //int _t;cin>>_t;while(_t--)  //to run solve() t no. of times where t is no. of testcases
+    solve();
+}
