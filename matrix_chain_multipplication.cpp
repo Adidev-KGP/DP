debe23b1f0e1c97c9b98ef717c0b0bd165aaf6e5
Adidev-KGP <74253561+Adidev-KGP@users.noreply.github.com>
2022-06-02 01:45:57 +0530

    Create matrix_chain_multipplication.cpp

---
+++ matrix_chain_multipplication.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int mcm(int arr[],int i,int j){
+    if(i>=j)return 0;
+    
+    int ans=INT_MAX;
+    int tempans;
+    int k;
+    
+    for(k=i;k<=j-1;k++){
+        tempans=mcm(arr,i,k)+mcm(arr,k+1,j)+arr[i-1]*arr[k]*arr[j];
+    
+    if(tempans<ans)ans=tempans;
+    }
+    return ans;
+    
+}
+
+signed main(){
+   ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);//FASTIO
+   int n;cin>>n;
+   int arr[n];
+   for(int i=0;i<n;i++)cin>>arr[i];
+   cout<<mcm(arr,1,n-1);
+}
