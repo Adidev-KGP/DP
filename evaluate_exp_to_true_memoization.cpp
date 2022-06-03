682bc79a706d9dea02f4d2ebd05337efba607767
Adidev-KGP <74253561+Adidev-KGP@users.noreply.github.com>
2022-06-03 21:31:18 +0530

    Create evaluate_exp_to_true_memoization.cpp

---
+++ evaluate_exp_to_true_memoization.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+unordered_map<string,int>mp;
+
+int solve(string s,int i,int j,bool isTrue){
+    if(i>j)return false;
+    if(i==j){
+        if(isTrue==true) return s[i]=='T';
+        else return s[i]=='F';
+    }
+    
+    string temp = to_string(i)+" "+to_string(j)+" "+to_string(isTrue);
+    
+    if(mp.find(temp)!=mp.end())return mp[temp];
+    
+    
+    int ans = 0;
+    for(int k=i+1;k<=j-1;k+=2){
+        int lt = solve(s,i,k-1,true);
+        int lf = solve(s,i,k-1,false);
+        int rt = solve(s,k+1,j,true);
+        int rf = solve(s,k+1,j,false);
+        
+        if(s[k]=='&'){
+            if(isTrue)ans+= lt*rt;
+            else ans+= lf*rt + lt*rf + lf*rf;
+        }else if(s[k]=='|'){
+            if(isTrue)ans+= lt*rf + lf*rt + lt*rt;
+            else ans+= lf*rf;
+        }else if(s[k]=='^'){
+            if(isTrue)ans+= lf*rt + lt*rf;
+            else ans+= lt*rt + lf*rf;
+        }
+        
+    }return mp[temp]=ans;
+    
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);//FASTIO
+    mp.clear();
+    //int _t;cin>>_t;while(_t--)  //to run solve() t no. of times where t is no. of testcases
+    string s;cin>>s;
+    cout<<solve(s,0,s.length()-1,true);
+}
