be3d68fef790f0b17090b1fe7b1650f4fbbf375d
Adidev-KGP <74253561+Adidev-KGP@users.noreply.github.com>
2022-06-09 08:51:31 +0530

    Update knapsack_0-1.cpp
    Input:
    wt[] : {1, 3, 4, 5}
    val[] : {1 ,4 ,5 ,7}
    n=4
    W=7

--- knapsack_0-1.cpp
+++ knapsack_0-1.cpp
@@ -28,7 +28,6 @@ void solve(){
 
 signed main(){
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);//FASTIO
-    
     int _t;cin>>_t;while(_t--)  //to run solve() t no. of times where t is no. of testcases
     solve();
 }
